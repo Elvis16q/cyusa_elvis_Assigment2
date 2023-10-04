9a0f0e3626adaf698bf97c597b692d25026f2003
Elvis16q <137180945+Elvis16q@users.noreply.github.com>
2023-10-04 22:51:20 +0200

    Add files via upload
    cyusa elvis 24110

---
+++ project/src/com/auca/service/HelloWord.java
@@ -0,0 +1,36 @@
+package com.auca.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/signup")
+public class HelloWord extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {	
+	
+		 String username = req.getParameter("name");
+	        String password = req.getParameter("password");
+
+	        if ("cyusa elvis".equals(username) && "player11".equals(password)) {
+	            try {
+	            	HttpSession session = req.getSession(true);
+	            	session.setAttribute("name", username);
+	                res.sendRedirect("StudentAdmission.html");
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        } else {            
+	            res.sendRedirect("forgetPassword.html"); 
+	        }
+	
+	
+	
+	
+	
+	}
+}
